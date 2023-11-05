b2edb8efde7900ca8aa2d143115b7261b2d35160
gabrielgcma <amadocmgabriel@gmail.com>
2023-11-05 17:45:39 -0300

    Merging gabriel -> main

--- future/src/frontend/src/api/root.js
+++
@@ -1,1 +0,0 @@
-export const root = "http://127.0.0.1:8000/";
--- future/src/frontend/src/pages/Home.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div class="flex w-screen h-screen">
-      <h1 class="font-custom text-white text-3xl font-bold mx-auto pt-20">
-        Avaliação de docentes e disciplinas aberta e interativa
-      </h1>
-    </div>
-  );
-}
